00ca9ce881cfc62b11f2957b8b5c491cec494cfa
russik83 <russik27@mail.ru>
2020-03-27 15:30:14 +0300

    Образец

--- webapp/model.py
+++ webapp/model.py
@@ -4,16 +4,18 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
+
 class News(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     url = db.Column(db.String, unique=True, nullable=False)
     published = db.Column(db.DateTime, nullable=False)
     text = db.Column(db.Text, nullable=True)
-    
+
     def __repr__(self):
         return '<News {} {}>'.format(self.title, self.url)
-        
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 
 from webapp.model import db, News
 
+
 def get_html(url):
     try:
         result = requests.get(url)
@@ -14,15 +15,16 @@ def get_html(url):
         print('Сетевая ошибка')
         return False
 
+
 def get_python_news():
     html = get_html("https://www.python.org/blogs/")
     if html:
-        soup = BeautifulSoup(html, 'html.parser')
+        soup = BeautifulSoup(html, "html.parser")
         all_news = soup.find('ul',class_='list-recent-posts').findAll('li')
         result_news = []
         for news in all_news:
             title = news.find('a').text
-            url =  news.find('a')['href']
+            url = news.find('a')['href']
             published = news.find('time').text
             try:
                 published = datetime.strptime(published, '%Y-%m-%d')
@@ -30,10 +32,11 @@ def get_python_news():
                 published = datetime.now()
             save_news(title, url, published)
 
+
 def save_news(title, url, published):
     news_exists = News.query.filter(News.url == url).count()
     print(news_exists)
-    if not news_exists:  
+    if not news_exists:
         new_news = News(title=title, url=url, published=published)
         db.session.add(new_news)
         db.session.commit()
